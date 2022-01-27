f2d9ef8e9cdcfce2e2f910f3e7c6910217ed3152
Manumission247 <edbingham92@gmail.com>
2022-01-27 22:30:43 +0000

    Join Channels

--- TwitchBot/index.js
+++ TwitchBot/index.js
@@ -15,17 +15,19 @@ module.exports = discordClient => {
     const BotEmojis = require("../botconfig/emojis.json");
     const BotEmbed = require("../botconfig/embed.json");
     var Filter = require('bad-words'),
-    filter = new Filter();
+        filter = new Filter();
     // Define configuration options
     const twitchsql = new SQLite(`./databases/twitch.sqlite`);
     const twitchsqldata = twitchsql.prepare("SELECT * FROM twitch").all();
 
-    var channels = "";
     for (const data of twitchsqldata) {
         var twitchchat = data.twitch;
-        var channels = channels + "" + twitchchat + ",\n";
+        client.join(twitchchat).then((data) => {
+            client.say(channel, `I am now moderating this channel`);
+        }).catch((err) => {
+            client.log.warn(`Join Error ${err}`);
+        });
     }
-    console.log(channels);
     const opts = {
         connection: {
             reconnect: true
@@ -35,76 +37,84 @@ module.exports = discordClient => {
             password: settings.password,
         },
         channels: [
-            channels
+            "modmanagerbot",
         ]
-    };
+};
 
-    // Create a client with our options
-    const client = new tmi.client(opts);
+// Create a client with our options
+const client = new tmi.client(opts);
 
-    // Register our event handlers (defined below)
-    client.on('connected', onConnectedHandler);
+// Register our event handlers (defined below)
+client.on('connected', onConnectedHandler);
 
-    // Connect to Twitch:
-    client.connect();
-    client.on('join', (channel, username, self) => {
-        if(self) {
-            client.log.warn(`Joined ${channel}`);
-        }
-    });
-    client.on('message', (channel, userstate, message, self) => {
-        console.log("I saw a message");
-        // Ignore echoed messages.
-        if (self) return;
-        if (message.toLowerCase() === 'hello') {
-            // "@alca, heya!"
-            client.say(channel, `@${userstate.username}, heya!`);
-        } else if (message.toLowerCase() == '!dice') {
-            const num = rollDice();
-            client.say(channel, `You rolled a ${num}`);
-        }else if (message.toLowerCase() == '!discord') {
-            client.say(channel, `We are working on this at the moment!`);
-        }
-        checkTwitchChat(userstate, message, channel)
-    });
-
-
-    function checkTwitchChat(username, message, channel) {
-        message = message.toLowerCase();
-        let shouldSendMessage = false
-        // check message
-        shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLowerCase()))
-        // None added by Bot Admin now check bad-words filter
-        if(shouldSendMessage == false) {
-            const forbidenWords = ['shit', 'bollocks', 'twat', 'nigger', 'bastard', 'cunt', '.xxx', 'XX'];
-            var customFilter = new Filter({ placeHolder: 'XX' });
-
-              const msg = customFilter.clean(message);
-              for (var i = 0; i < forbidenWords.length; i++) {
-                shouldSendMessage = msg.includes(forbidenWords[i]);
-              }
-           
-        }
-        if (shouldSendMessage) {
-            //tell user
-            client.say(channel, `@${username.username}, sorry, that message is not permitted here!`)
-            //Delete Message
-            client.deletemessage(channel, message.identity).then((data) => {
-                // data returns
-            }).catch((err) => {
-                console.log(err);
-            })
-        }
+// Connect to Twitch:
+client.connect();
+client.on('join', (channel, username, self) => {
+    if (self) {
+        client.log.warn(`Joined ${channel}`);
     }
-    // Function called when the "dice" command is issued
-    function rollDice() {
-        const sides = 6;
-        return Math.floor(Math.random() * sides) + 1;
+});
+client.on("part", (channel, username, self) => {
+    // Do your stuff.
+    client.log.warn(`Left ${channel}`);
+});
+client.on("slowmode", (channel, enabled, length) => {
+    // Do your stuff.
+    client.say(channel, `Channel SLOW MODE: ${enabled}`);
+});
+client.on('message', (channel, userstate, message, self) => {
+    console.log("I saw a message");
+    // Ignore echoed messages.
+    if (self) return;
+    if (message.toLowerCase() === 'hello') {
+        // "@alca, heya!"
+        client.say(channel, `@${userstate.username}, heya!`);
+    } else if (message.toLowerCase() == '!dice') {
+        const num = rollDice();
+        client.say(channel, `You rolled a ${num}`);
+    } else if (message.toLowerCase() == '!discord') {
+        client.say(channel, `We are working on this at the moment!`);
     }
+    checkTwitchChat(userstate, message, channel)
+});
 
-    // Called every time the bot connects to Twitch chat
-    function onConnectedHandler(addr, port) {
-        console.log(`* Connected to ${addr}:${port}`);
+
+function checkTwitchChat(username, message, channel) {
+    message = message.toLowerCase();
+    let shouldSendMessage = false
+    // check message
+    shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLowerCase()))
+    // None added by Bot Admin now check bad-words filter
+    if (shouldSendMessage == false) {
+        const forbidenWords = ['shit', 'bollocks', 'twat', 'nigger', 'bastard', 'cunt', '.xxx', 'XX'];
+        var customFilter = new Filter({ placeHolder: 'XX' });
+
+        const msg = customFilter.clean(message);
+        for (var i = 0; i < forbidenWords.length; i++) {
+            shouldSendMessage = msg.includes(forbidenWords[i]);
+        }
 
     }
+    if (shouldSendMessage) {
+        //tell user
+        client.say(channel, `@${username.username}, sorry, that message is not permitted here!`)
+        //Delete Message
+        client.deletemessage(channel, message.identity).then((data) => {
+            // data returns
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+}
+// Function called when the "dice" command is issued
+function rollDice() {
+    const sides = 6;
+    return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler(addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+
+}
 }
