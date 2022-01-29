1547c3067a182e48b5e135d9741e66d6b3fc00a1
Manumission247 <edbingham92@gmail.com>
2022-01-29 20:15:35 +0000

    Join Message

--- commands/TwitchBot/join.js
+++ commands/TwitchBot/join.js
@@ -14,48 +14,25 @@ module.exports = {
     alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
     run: async (client, message, args) => {
         try {
-        var chan = channel.substring(1);
-        const twitchchannel = twitchsql.prepare("SELECT * FROM twitch WHERE twitch = ?").all(chan);
-        var twitchdata = "";
-        for (const data of twitchchannel) {
-            if (data == undefined) {
-                twitchdata = "";
-            } else {
-                discord = data.guild;
-                discordClient.features.ensure(discord, {
-                    music: true,
-                    logs: true,
-                    reactionroles: true,
-                    moderation: true,
-                    fun: true,
-                    youtube: false,
-                    support: true,
-                    points: true,
-                    TwitchFilter: true,
-                    twitchbot: true,
-                });
-            }
-            if (discordClient.features.get(discord, "twitchbot") == true) {
-                const { joinChannel } = require(`../../TwitchBot/index`);
-                const guild = message.guild.id;
-                const twitchsql = new SQLite(`./databases/twitch.sqlite`);
-                const twitchchannel = twitchsql.prepare(`SELECT * FROM twitch WHERE guild = ?`).all(guild);
+            const { joinChannel } = require(`../../TwitchBot/index`);
+            const guild = message.guild.id;
+            const twitchsql = new SQLite(`./databases/twitch.sqlite`);
+            const twitchchannel = twitchsql.prepare(`SELECT * FROM twitch WHERE guild = ?`).all(guild);
 
-                for (const data of twitchchannel) {
-                    var twitchchat = data.twitch;
-                    console.log(twitchchat);
-                    if (twitchchat) {
-                        joinChannel(twitchchat);
-                        message.reply(`Joined channel: ${twitchchat}`);
-                    } else {
-                        message.reply("There is no twitch channel set in the dashboard?");
-                    }
+            for (const data of twitchchannel) {
+                var twitchchat = data.twitch;
+                console.log(twitchchat);
+                if (twitchchat) {
+                    joinChannel(twitchchat);
+                    message.reply(`Joined channel: ${twitchchat}`);
+                } else {
+                    message.reply("There is no twitch channel set in the dashboard?");
                 }
+
             }
+        } catch (e) {
+            const { logMessage } = require(`../../handlers/newfunctions`);
+            logMessage(client, `error`, message.guild, `Error with JOIN command: ${e.message} | \`\`\` ${e.stack} \`\`\``);
         }
-    } catch (e) {
-        const { logMessage } = require(`../../handlers/newfunctions`);
-        logMessage(client, `error`, message.guild, `Error with JOIN command: ${e.message} | \`\`\` ${e.stack} \`\`\``);
-    }
     }
 }
