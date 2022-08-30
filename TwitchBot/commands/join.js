4bb2334d7fd8ff8e3e5c0b9521f50e2110dd2d2d
Manumission247 <manumission247@gmail.com>
2022-08-30 17:45:37 +0100

    V4 Initial

--- TwitchBot/commands/join.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, channel, userstate, message, self, args) => {
-        try {
-            client.join($arg[0]);
-            console.log(`Joined the channel: ${arg[0]}`);
-            client.say(channel, `Joined the channel: ${arg[0]}`)
-        } catch (e) {
-            console.log(e);
-        }
-}
--- TwitchBot/commands/ping.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client, channel, userstate, message, self, args) => {
-    client.ping().then(function(data) {
-        let ping = Math.floor(Math.round(data*1000))
-        client.say(channel, `@${userstate.username}, your ping is ${ping}`)
-    })
-}
