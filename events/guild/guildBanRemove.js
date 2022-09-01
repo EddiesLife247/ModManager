2f8555d619ec2fb4e2d707165b5fb98273cbceb6
Manumission247 <manumission247@gmail.com>
2022-09-01 13:22:47 +0100

    AuditEvent Logs - Added to Database when banned, and removed when deleted from server.

--- events/guild/guildBanRemove.js
+++ events/guild/guildBanRemove.js
@@ -10,18 +10,19 @@ const Discord = require(`discord.js`);
 const SQLite = require("better-sqlite3");
 const bansql = new SQLite(`./databases/bans.sqlite`);
 const botsql = new SQLite(`./databases/bot.sqlite`);
-module.exports = async (client, channel) => {
+module.exports = async (client, member) => {
+    console.log("Ban Removed");
     try {
-    const guild = channel.guild;
-    if (channel.guild.members.me.permissions.has(PermissionsBitField.Flags.ViewAuditLog)) {
+    const guild = member.guild;
+    if (member.guild.members.me.permissions.has(PermissionsBitField.Flags.ViewAuditLog)) {
             try {
-                client.logchannel = botsql.prepare(`SELECT settings.settingValue FROM settings WHERE setting = 'logchannel' AND guildid = '${channel.guild.id}'`);
-                console.log(`SELECT settings.settingValue FROM settings WHERE setting = 'logchannel' AND guildid = ${channel.guild.id}`);
-                console.log(client.logchannel.get());
+                client.logchannel = botsql.prepare(`SELECT settings.settingValue FROM settings WHERE setting = 'logchannel' AND guildid = '${member.guild.id}'`);
+                //console.log(`SELECT settings.settingValue FROM settings WHERE setting = 'logchannel' AND guildid = ${member.guild.id}`);
+                //console.log(client.logchannel.get());
             //if (channel.me.permissions.has("VIEW_AUDIT_LOG")) {
-            const fetchedLogs = await channel.guild.fetchAuditLogs({
+            const fetchedLogs = await member.guild.fetchAuditLogs({
                 limit: 1,
-                type: AuditLogEvent.ChannelCreate,
+                type: AuditLogEvent.MemberBanRemove,
             });
             const chLog = fetchedLogs.entries.first();
             if (Date.now() - chLog.createdTimestamp < 5000) {
@@ -30,18 +31,20 @@ module.exports = async (client, channel) => {
                 }
                 const { executor, target } = chLog;
                 var execute = executor.tag;
+                var targ = target.username;
             }
             else {
                 var execute = "UNKNOWN";
             }
+            //console.log(chLog);
             //console.log(channel.guild.channels.cache.find(c => c.name == settings.modLogChannel));
-            const logchannel = channel.guild.channels.cache.get(client.logchannel.get().settingValue);
-            console.log(logchannel.id);
+            const logchannel = member.guild.channels.cache.get(client.logchannel.get().settingValue);
+            //console.log(logchannel.id);
             const embed = new EmbedBuilder();
                 embed.setColor("#00ff00")
-                embed.setTitle('**MODERATION LOG: Channel Created**');
+                embed.setTitle('**MODERATION LOG: Member Unbanned**');
                 embed.addFields(
-                    { name: 'Channel Name::', value: `${channel.name}`, inline: true },
+                    { name: 'Member UnBanned:', value: `${targ}`, inline: true },
                     { name: 'Executor', value: `${execute}`, inline: false },
                 )
                 embed.setTimestamp();
@@ -49,6 +52,13 @@ module.exports = async (client, channel) => {
                logchannel.send({ embeds: [embed] });
             //console.log(`pin updated in a guild that has logs enabled!`);
             //}
+            const bansql = new SQLite(`./databases/bans.sqlite`);
+            client.getBanned = bansql.prepare("SELECT * FROM bans WHERE user = ? AND guild = ?");
+            let globalBanned;
+            globalBanned = client.getBanned.get(member.user.id, member.guild.id);
+            if (globalBanned) {
+                bansql.prepare(`DELETE FROM 'bans' WHERE user = '${member.user.id}' AND guild = '${member.guild.id}'`).run()
+            }
         }
         catch (err) {
             console.log(err);
