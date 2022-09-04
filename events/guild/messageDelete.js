02608542a75e7f846cfa7ca6b1fd0e6e0f6e0307
Manumission247 <manumission247@gmail.com>
2022-09-04 17:57:34 +0100

    Message Delete Bug

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -3,51 +3,53 @@ const ms = require('ms');
 const config = require('../../configs/config.json');
 const SQLite = require("better-sqlite3");
 var Filter = require('bad-words'),
-	filter = new Filter();
+    filter = new Filter();
 const cooldown = new Collection();
 const scoresql = new SQLite(`./databases/scores.sqlite`);
 const bansql = new SQLite(`./databases/bans.sqlite`);
 const botsql = new SQLite(`./databases/bot.sqlite`);
 module.exports = async (client, message) => {
-	try {
-		client.setup = botsql.prepare(`SELECT * FROM settings WHERE guildid = '${message.guild.id}'`);
-		if (!client.setup.all().length) {
-			console.log(`${message.guild.name} - Is not setup!`);
-			return;
-		}
+    try {
+        client.setup = botsql.prepare(`SELECT * FROM settings WHERE guildid = '${message.guild.id}'`);
+        if (!client.setup.all().length) {
+            console.log(`${message.guild.name} - Is not setup!`);
+            return;
+        }
         client.logchannel = botsql.prepare(`SELECT logchannel FROM settings WHERE guildid = '${message.guild.id}'`);
         if (client.logchannel.get().logchannel) {
             const logchannel = message.guild.channels.cache.get(client.logchannel.get().logchannel);
-			const guild = message.guild;
+            const guild = message.guild;
             if (message.guild.members.me.permissions.has(PermissionsBitField.Flags.ViewAuditLog)) {
-				// LOGS HERE
-				try {
+                // LOGS HERE
+                try {
                     const fetchedLogs = await message.guild.fetchAuditLogs({
                         limit: 1,
                         type: AuditLogEvent.MessageDelete,
                     });
                     const chLog = fetchedLogs.entries.first();
-					//console.log(chLog);
+                    //console.log(chLog);
                     if (Date.now() - chLog.createdTimestamp < 5000) {
                         if (!chLog) {
                             var execute = "UNKNOWN";
+                            var author = target.id;
+                        } else {
+                            const { executor, target } = chLog;
+                            var execute = executor.username;
+                            console.log(target);
                         }
-                        const { executor, target } = chLog;
-                        var execute = executor.username;
-                        console.log(target);
-						var author = target.ClientUser.id;
+
                     }
                     else {
                         var execute = "UNKNOWN";
-						var author = "UNKNOWN";
+                        var author = "UNKNOWN";
                     }
                     const embed = new EmbedBuilder();
                     embed.setColor("#00ff00")
                     embed.setTitle('**MODERATION LOG: Message Deleted**');
                     embed.addFields(
-						{ name: 'Channel:', value: `<#${message.channel.name}>`, inline: true },
-						{ name: 'Author:', value: `<#${author}>`, inline: true },
-						{ name: 'Deleted by:', value: `${execute}`, inline: false },
+                        { name: 'Channel:', value: `<#${message.channel.name}>`, inline: true },
+                        { name: 'Author:', value: `<#${author}>`, inline: true },
+                        { name: 'Deleted by:', value: `${execute}`, inline: false },
                     )
                     embed.setTimestamp();
 
@@ -59,11 +61,11 @@ module.exports = async (client, message) => {
                     client.guilds.cache.get("787871047139328000").channels.cache.get("901905815810760764").send({ content: `ERROR: event: ${err.message} | \`\`\` ${err.stack} \`\`\`` });
                     return;
                 }
-				
-			}
-		}
 
-	} catch (err) {
+            }
+        }
+
+    } catch (err) {
         console.log(err);
         client.guilds.cache.get("787871047139328000").channels.cache.get("901905815810760764").send({ content: `ERROR: event: ${err.message} | \`\`\` ${err.stack} \`\`\`` });
         return;
