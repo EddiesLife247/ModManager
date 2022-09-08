323fbfed4ea1990b091304468ecddcadde23e8ea
Manumission247 <manumission247@gmail.com>
2022-09-08 21:18:40 +0100

    bulkDelete

--- slashCommands/moderation/bulkdelete.js
+++ slashCommands/moderation/bulkdelete.js
@@ -19,39 +19,14 @@ module.exports = {
             min_value: 1,
             max_value: 100,
         },
-        {
-            name: 'user',
-            description: 'Delete by a user',
-            type: 6,
-            required: false,
-        },
     ],
     run: async (client, interaction) => {
         const amount = interaction.options.getInteger('amount');
-        if (interaction.options.get('user')) {
-            const user = interaction.guild.members.cache.get(interaction.options.get('user').value);
-            interaction.channel.messages.fetch({
-                limit: 100,
-            }).then((messages) => {
-                if (user) {
-                    const filterBy = user ? user.id : client.user.id;
-                    messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-                }
-                interaction.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-                interaction.reply({content: `I have deleted ${amount} messages from this channel by user: ${user.username}.`, ephernal: true})
-
-            })
-        } else {
-            interaction.channel.messages.fetch({
-                limit: 100,
-            }).then((messages) => {
-                interaction.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-                interaction.reply({content: `I have deleted ${amount} messages from this channel.`, ephernal: true})
-
-            });
-        }
-        console.log(amount);
-
-
+        interaction.channel.messages.fetch({
+            limit: 100,
+        }).then((messages) => {
+            interaction.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+            interaction.reply({ content: `I have deleted ${amount} messages from this channel.`, ephernal: true })
+        });
     }
 }
