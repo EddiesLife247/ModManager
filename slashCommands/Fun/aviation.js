24a5a3979cb349cbe4d4307bb94a404ca8f68488
Manumission247 <manumission247@gmail.com>
2022-09-22 09:31:15 +0100

    aviation command

--- slashCommands/Fun/aviation.js
+++ slashCommands/Fun/aviation.js
@@ -32,7 +32,50 @@ module.exports = {
         interaction.deferReply();
         if (interaction.options.get('taf')) {
             const tafICAO = interaction.options.get('taf').value;
-
+            try {
+                console.log(`METAR for ${tafICAO} by ${message.author.tag}`);
+                let argz = tafICAO.map(e => e.toUpperCase());
+                let url = `https://avwx.rest/api/taf/${argz}?options=info,translate,speech&token=I6DF0eFZdyAOfL6th2UVZsAWb_Cei_iAurasC8hVN7o`;
+                console.log(url);
+                https.get(url, (res) => {
+                    let body = "";
+                    res.on("data", (chunk) => {
+                        body += chunk;
+                    });
+                    res.on("end", () => {
+                        try {
+                            let json = JSON.parse(body);
+                            //console.log(json);
+                            // do something with JSON
+                            let MetaEmbed = new Discord.MessageEmbed()
+                                .setTitle(`${argz} TAF INFORMATON`)
+                                .addField('Name: ', `> ${json["info"]["name"]}`, true)
+                                .addField('Type: ', `> ${json["info"]["type"]}`, true)
+                                .addField('Web: ', `>  ${json["info"]["website"]}`, true)
+                                .addField('City: ', `> ${json["info"]["city"]}`, true)
+                                .addField('Country: ', `> ${json["info"]["country"]}`, true)
+                                .addField('Timestamp: ', `> ${json["meta"]["timestamp"]}`, true)
+                                .addField('Station: ', `>  ${json["station"]}`, true)
+                                .addField('Forecast: ', `> ${json["speech"]}`, true)
+                                .addField('Raw Data', `>  ${json["raw"]}`, false)
+                                .addField('Sanitized Data', `> ${json["sanitized"]}`, false)
+                                .setColor([0, 255, 0])
+                            //.setURL(json[`info`][`website`]);
+                            interaction.editReply({ embeds: [MetaEmbed] });
+        
+                        } catch (error) {
+                            console.error(error.message);
+                            interaction.editReply(error.message);
+                        };
+                    });
+        
+                }).on("error", (error) => {
+                    console.error(error.message);
+                });
+            } catch (err) {
+                const { logMessage } = require(`../../handlers/newfunctions`);
+                client.guilds.cache.get("787871047139328000").channels.cache.get("901905815810760764").send({ content: `Error with TAF command: ${message} | ${err.message} | \`\`\` ${err.stack} \`\`\``});
+            }
         }
         if (interaction.options.get('metar')) {
             const metarICAO = interaction.options.get('metar').value;
