22f3d11f5ff96b85dc9d7b2665bc649b79ec2a6a
Manumission247 <manumission247@gmail.com>
2022-09-24 13:18:08 +0100

    Return of Activites

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,11 +2,11 @@ const chalk = require('chalk')
 
 module.exports = client => {
 	const activities = [
-		/*{ name: `${client.guilds.cache.size} Servers`, type: 2 }, // LISTENING
+		{ name: `${client.guilds.cache.size} Servers`, type: 2 }, // LISTENING
 		{ name: `${client.channels.cache.size} Channels`, type: 0 }, // PLAYING
 		{ name: `${client.users.cache.size} Users`, type: 3 }, // WATCHING
-		{ name: `Now on Version 4 | Run /config to SETUP`, type: 1 } // COMPETING
-		*/
+		{ name: `Now on Version 4 | Run /config to SETUP`, type: 1 }, // COMPETING
+		
 		{ name: `RIP HM Queen Elizabeth - 1926 - 2022`, type: 0 }
 	];
 	const status = [
